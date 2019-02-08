770aa4d72057cf20a5729313761be7d36037ba78
Youngermaster <jmyoungh@eafit.edu.co>
2019-02-08 17:33:11 +0100

    Third commit

--- lib/Talleres/Taller 2/scanner.dart
+++ lib/Talleres/Taller 2/scanner.dart
@@ -2,9 +2,20 @@ import 'dart:io';
 
 /**
  *
- * returns a integer
+ * returns a double.
  */
 readDouble(String message) {
+  print(message);
+  var read = stdin.readLineSync().trim();
+  return double.parse(read);
+}
+
+
+/**
+ *
+ * returns a integer.
+ */
+readInteger(String message) {
   print(message);
   var read = stdin.readLineSync().trim();
   return double.parse(read);
--- lib/Talleres/Taller 2/shipPartitionProblem.dart
+++ lib/Talleres/Taller 2/shipPartitionProblem.dart
@@ -1,29 +1,39 @@
 import 'scanner.dart';
 
 main() {
-
-  _addProducts(6);
-  _chibchoGauss(_productList);
-  //_swinging();
-
-  _productList.forEach((iterable) => print(iterable.weight));
-  print("_" * 5);
-  _partOne.forEach((iterable) => print(iterable.weight));
-  print("_" * 5);
-  _partTwo.forEach((iterable) => print(iterable.weight));
+  shipPartitionProblemCore();
 }
 
-List<Product> _productList = List();
-List<Product> _partOne = List();
-List<Product> _partTwo = List();
+List productList = List();
+List _partOne = List();
+List _partTwo = List();
 
+/**
+ * Core
+ * returns void.
+ */
+
+shipPartitionProblemCore() {
+  _addProducts(30);
+  _bruteForce(productList);
+  _printFormatted();
+}
 
 /**
  *
- *
+ * returns void.
  */
-_getMiddle(List list) => _getListsSum(list) / 2;
 
+_printFormatted() {
+  productList.forEach((iterable) => print(iterable));
+  print("_" * 5);
+
+  _partOne.forEach((iterable) => print(iterable));
+  print("* ${_getListsSum(_partOne)}");
+  print("_" * 5);
+  _partTwo.forEach((iterable) => print(iterable));
+  print("* ${_getListsSum(_partTwo)}");
+}
 /**
  *
  * returns void
@@ -31,109 +41,47 @@ _getMiddle(List list) => _getListsSum(list) / 2;
 _addProducts(int amount) {
   int counter = 0;
   while (counter < amount) {
-    double weight = readDouble("Enter the weight of the product ${counter + 1}, please: ");
-    var newProduct = Product("Product ${counter + 1}", weight);
-    _productList.add(newProduct);
+    double weight = readDouble("Enter the weight of the product, please: ");
+    productList.add(weight);
     counter++;
   }
 }
 
 /**
  *
- *  returns void.
- */
-_chibchoGauss(List<Product> list) {
-  List weights = List();
-
-  for (Product products in list)
-      weights.add(products.weight);
-
-  weights.sort();
-  print(weights);
-  int leftCounter;
-  int rightCounter = list.length - 1;
-  double halfNumber = _getMiddle(weights);
-
-  var leftSum = 0.0;
-  var rightSum = 0.0;
-
-  if (list.length % 2 == 0) {
-    for (leftCounter = 0; leftCounter < (list.length / 2) - 1; leftCounter += 2) {
-      _partOne.add(list[leftCounter]);
-      leftSum += weights[leftCounter];
-      _partOne.add(list[rightCounter]);
-      leftSum += weights[rightCounter];
-
-      rightCounter -= 2;
-    }
-
-    rightCounter = list.length - 2;
-
-    for (leftCounter = 1; leftCounter < (list.length / 2); leftCounter += 2) {
-      _partTwo.add(list[leftCounter]);
-      rightSum += weights[leftCounter];
-      _partTwo.add(list[rightCounter]);
-      rightSum += weights[rightCounter];
-
-      rightCounter -= 2;
-    }
-
-    print(leftSum);
-    print(rightSum);
-
-    bool booleanOne = leftSum + weights[((list.length ~/2) - 1)] == halfNumber;
-    bool booleanTwo = rightSum + weights[((list.length ~/2) - 1)] == halfNumber;
-    bool booleanThree = rightSum + weights[(list.length ~/2)] == halfNumber;
-    bool booleanFour = leftSum + weights[(list.length ~/ 2)] == halfNumber;
-    if (booleanOne && booleanThree) {
-      _partOne.add(list[list.indexOf(((weights.length ~/2) - 1))]);
-      _partTwo.add(list[(list.length ~/2)]);
-      print("I'm here");
-    } else if(booleanTwo && booleanFour) {
-      print("WTF!");
-      _partOne.add(list[(list.length ~/2)]);
-      _partTwo.add(list[((list.length ~/2) - 1)]);
-    } else {
-      print("There are not solution");
-    }
-
-  }
-
-}
-
-/**
- *
  * returns void.
  */
-_swinging() {
-  double weightRight = 0;
-  double weightLeft = 0;
-
-  for (Product product in _productList) {
-    if (weightRight - product.weight >= weightLeft - product.weight) {
-      _partOne.add(product);
-      weightLeft += product.weight;
-    }
-    else {
-      _partTwo.add(product);
-      weightRight += product.weight;
+_bruteForce(List list) {
+  double balanceNumber = _getBalanceNumber(list);
+  mainLoop: for (int iterator = 0; iterator < list.length; iterator++) {
+    _partOne.add(list[iterator]);
+    if (_getListsSum(_partOne) == balanceNumber) {
+      _partTwo = list.sublist(iterator++);
+      if (_getListsSum(_partTwo) == balanceNumber) {
+        break mainLoop;
+      }
+      list.shuffle();
+      _partOne.clear();
+      iterator = 0;
+      continue mainLoop;
+    } else if( _getListsSum(_partOne) > balanceNumber) {
+      list.shuffle();
+      _partOne.clear();
+      iterator = 0;
+      continue mainLoop;
     }
   }
 }
 
 /**
  *
- * returns double
+ *  returns null if the list is empty else it will
  */
-_getListsSum(List list) => list.reduce((a, b) => a + b);
+_getListsSum(List list) => list.isEmpty ? null : list.reduce((a, b) => a + b);
+
 
 /**
- * Class product
- * contains...
+ *
+ *  returns double.
  */
-class Product {
- String name;
- double weight;
-
- Product(this.name, this.weight);
-}
+_getBalanceNumber(List list) => _getListsSum(list) / 2;
