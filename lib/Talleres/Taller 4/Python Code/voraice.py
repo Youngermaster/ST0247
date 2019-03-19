813eb11ac777076e4315485925ee9e3542a136ed
Youngermaster <jmyoungh@eafit.edu.co>
2019-03-18 19:22:51 -0500

    I have added Pickachu

--- lib/Talleres/Taller 4/Python Code/voraice.py
+++ lib/Talleres/Taller 4/Python Code/voraice.py
@@ -37,7 +37,7 @@ def read_csv(file_path):
                 optional[line[1]] = int(line[2])
 
 
-def assign_limit_by_elements(list, limit):
+def assign_limit_by_elements_first_budget(list, limit):
     auxiliary_list = sorted(list.items(), key=operator.itemgetter(1))
     products = []
     prices = []
@@ -60,47 +60,7 @@ def assign_limit_by_elements(list, limit):
         iterator += 1
     
 
-def ultra_core(list, limit):
-    auxiliary_list = sorted(list.items(), key=operator.itemgetter(1))
-    products = []
-    prices = []
-
-    for iterator in range(len(auxiliary_list)):
-        products.append(auxiliary_list[iterator][0])
-        prices.append(auxiliary_list[iterator][1])
-    
-    itemWeight = []
-
-    for i in range(len(products)):
-        itemWeight.append(1)
-
-    items = [itemWeight, prices]
-    backpack(limit, items)
-
-def backpack(maxWeight, items):
-    matrix = [[0 for col in range(maxWeight+1)] for row in range(len(items[0]))]
-
-    for row in range(len(items[0])):  # going through the matrix we set up
-        for col in range(maxWeight+1):
-            if items[0][row] > col:  # this is the check to see if the item can go into our knap-sack
-                matrix[row][col] = matrix[row-1][col]  # get the item above and place
-            else:
-                matrix[row][col] = max(matrix[row-1][col], matrix[row-1][col-items[0][row]]+ items[1][row])
-    for i in range(len(items[0])):
-        print(matrix[i])
-    packed = []
-    col = maxWeight
-    # checking to see which items will go into our knapsack
-    for row in range(len(items[0])-1,-1,-1):
-        if row == 0 and matrix[row][col] != 0:
-            packed.insert(0,row)
-        if matrix[row][col] != matrix[row-1][col]:
-            packed.insert(0,row)
-            col -= items[0][row]
-    print(packed)
-    print('Max value is ', matrix[len(items[0])-1][maxWeight])
-
-def assign_optionals():
+def assign_optionals_first_budget():
     auxiliary_list = sorted(optional.items(), key=operator.itemgetter(1))
     products = []
     prices = []
@@ -129,20 +89,57 @@ def assign_optionals():
         iterator += 1
 
 
+def menu():
+    print("Estos son los productos que has pedido:\n")
+    for i in solutionBackPack:
+        print("-{}".format(i))
+    print("\n -> La cuenta es: {}".format(bill))
+    print("\n\tOh oh.")
+
+    print("""                  xOOOOOOOkdoc;                                                           ,,:cloo:'
+                  lOOOOOOOOOOOxoc;                                                    ,;cloxkOOOOl.
+		   xOOOOOOOOOOOOOxoc;                                              ;:loxkOOOOOOOOk:
+                   ck0OOOOO0OO00OOOOxl:                                       ,;codkO0OOOOOOOOOOOo'
+                    ck0OOOOOOOOOOOOO0Okoc;                               :cloxOOOOOOOOOOOOOOOO00;
+                     :xOOOOO0OOOOOOOOOOOkdc;                          cldkOOOOOOOOOOOOOOOOOOO0;
+                      ;dO0OOOOO0OOOOOOOOOOkdodddxxxxkkkOOOOOOOOOOkkkxkkO0OOOOOOOOOOOOOOOOOOO0'
+                       ,cxOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0000'
+                         ,lkOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0000;'
+                           ,lkOOkxkOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0000'
+                            .;lodkO0OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0000000;'
+                              ;xOOOOOOOOOOOO0OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO000000;'
+                              dOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOko;
+                             dOOOO0OOOxdoccxOO0OOOOOOOOOOOOOOOOOOOOxdoclxOOOOOOOOOOOo;
+                            lOOOOOOOOoo00c.;xOOOOOOOOOOOOOOOOOOOOOll0O:.:k0OOOOOOOOOOo,
+                           ckOOO0OO0Ol,::,.,d0OOOOOOOOOOOOOOOOOOOOc,::,.;x0OOOOOOOOOOkc'
+                          ;dOOOOOOOO0kc,,,;oOOO0OO0OO0OOOOOOOOOOOOkc,,,:dOOOOOOOOOOOOOd,
+                         ,lOOOOOOOOOOOOkxxkOOOOOOOOxdddkOOOOOOOOOOOOkxxOOOOOOOOOOOOOO0kc
+                         ;xOOOkkkOOOOOOOOOOOOOOOOOxo:;:dOOOOOOOOOOOOOOOOOOOOkkkkOOOOOOOd;
+                         ckxollllldkOOOOOOOOOOOOOO0OkkOOOOOOOOOOOOOOOOOOOkdlllllodkOOOOOc
+                         odccccccccokOOOOOOOOOOOOOOO0OOOOOOOOO0OOOOOOOOOxlcccccccclk0OOOd;
+                         ddcccccccclxOOOOOOOOOOOO            OOOOOOOOOOOdccccccccclx0OOOOl
+                         xOdolclllokOOOOOOOOOOOO              OOOOOOOOOOkocccccccldOOOOOOd
+			 lOOOkkkkOOOOOOOOOOOOOOO              OOOOOOOOOOOOkxdoddxkOOOOOOOOl
+                          dOOOOOOOOOOOOOOOOOOOO0              OOOOOOOOOOOOOOOOOOOOOOOOOOOOd
+                          cxOOOOOOOOOOOOOOOOOOOOOO           OOOOOOOOOOOOOOOOOOOOOOOOOOOOOk
+                           cxOOOOOOOOOOOOOOOOOOOOOOOOO     OO0OOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
+                            ck0OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOx
+    """)
+    print("\n\tEl sistema de tarjeta no funciona :'(")
+    
 
 
 def core():
     read_csv('E:\GIT\Github Projects\ST0247\lib\Talleres\Taller 4\Python Code\listacsv.csv')
-    assign_limit_by_elements(proteins, 8)
-    assign_limit_by_elements(flour, 10)
-    assign_limit_by_elements(grain, 3)
-    assign_limit_by_elements(toiletries, 4)
-    assign_limit_by_elements(oilsAndSauces, 3)
-    assign_limit_by_elements(vegetables, 12)
-    assign_optionals()
-    print(solutionBackPack)
-    print(bill)
-
+    assign_limit_by_elements_first_budget(proteins, 8)
+    assign_limit_by_elements_first_budget(flour, 10)
+    assign_limit_by_elements_first_budget(grain, 3)
+    assign_limit_by_elements_first_budget(toiletries, 4)
+    assign_limit_by_elements_first_budget(oilsAndSauces, 3)
+    assign_limit_by_elements_first_budget(vegetables, 12)
+    assign_optionals_first_budget()
+    menu()
+    
 if __name__ == "__main__":
     core()
 
