25e1947bdab4aca614598f2c1417a7d8c93b707a
Youngermaster <jmyoungh@eafit.edu.co>
2019-03-29 03:39:36 -0500

    I have added some changes

--- lib/Parcial/Parcial 1/parcial_1.py
+++ lib/Parcial/Parcial 1/parcial_1.py
@@ -5,7 +5,8 @@ from comuna import *
 bill = 0
 second_bill = 0
 
-
+name_of_comunas = set()
+comunas = []
 
 
 def read_csv(file_path):
@@ -13,20 +14,28 @@ def read_csv(file_path):
         lecture = csv.reader(datos_csv, delimiter=';')
         next(lecture, None)
         for line in lecture:
-            if line[0] == "Proteina":
-                proteins[line[1]] = int(line[2])
-            elif line[0] == "Harina":
-                flour[line[1]] = int(line[2])
-            elif line[0] == "Granos":
-                grain[line[1]] = int(line[2])
-            elif line[0] == "Aseo":
-                toiletries[line[1]] = int(line[2])
-            elif line[0] == "Aceite y salsa":
-                oilsAndSauces[line[1]] = int(line[2])
-            elif line[0] == "Vegetales":
-                vegetables[line[1]] = int(line[2])
-            else:
-                optional[line[1]] = int(line[2])
+            if line[1] == "Comuna":
+                print("->")
+                if line[2] in name_of_comunas:
+                    pass
+                else:
+                    comunas.append(Comuna(line[2]))
+                    name_of_comunas.add(line[2])
+
+def assign_people(comuna, age_range, people_quatity, gender):
+    if age_range == ' 0-4' or age_range == ' 5-9':
+        if gender == 'male':
+            comuna.boys += people_quatity
+        else:
+            comuna.girls += people_quatity    
+
+
+def sum(parameter_list):
+    pass
+
+
+def draw(parameter_list):
+    pass
 
 
 def menu():
@@ -34,12 +43,14 @@ def menu():
     
 
 def core():
-    #read_csv('PPMedellin2017.csv')
+    read_csv('PPMedellin2017.csv')
     #menu()
     print("\n\n")
-    c = Comuna("Name")
-    c.boys = "Juan"
-    print(c.boys)
+    #c = Comuna("Name")
+    #c.boys = "Juan"
+    #c.boys += " Manuel Young Hoyos"
+    #print(c.boys)
+    #print(c)
 
 
 
